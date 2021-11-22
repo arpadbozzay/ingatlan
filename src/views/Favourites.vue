d09c9380c83b1da5043f8982e3a5305664aa3e67
Arpad Bozzay <arpad.bozzay@xtendr.io>
2021-11-22 10:12:41 +0100

    [sorting added]

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -11,8 +11,24 @@
             </v-col>
           </v-row>
         </v-container>
+        <v-container elevation-2 class="sortContainer">
+          <v-row>
+            <v-col class="d-flex justify-start">
+              <v-btn @click="sortItems('uploadDate')">Date
+                <v-icon v-show="orderAsc" small>mdi-sort-calendar-ascending</v-icon>
+                <v-icon v-show="!orderAsc" small>mdi-sort-calendar-descending</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col class="d-flex justify-end">
+              <v-btn @click="sortItems('address')">Address
+                <v-icon v-show="orderAsc" small>mdi-sort-ascending</v-icon>
+                <v-icon v-show="!orderAsc" small>mdi-sort-descending</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
       </div>
-      <v-card class="my-3" v-for="data in adverts.ads" :key="data.adId">
+      <v-card class="my-3" v-for="data in orderedItems" :key="data.adId">
         <ListItem @adChanged="refresh" v-if="isFavourite(data.adId)" :showDate="true" :ad="data" />
       </v-card>
     </div>
@@ -22,10 +38,14 @@
 import ListItem from "../components/ListItem.vue";
 import json from '../assets/listings.json';
 const { isFavourite, getTotalFavourites } = require('../backend.js');
-
+const _ = require('lodash');
 
 export default {
     name: 'Favourites',
+
+    mounted(){
+      this.sortItems('uploadDate');
+    },
 
     components: {
       ListItem
@@ -34,9 +54,15 @@ export default {
      data: () => ({
       selectedItem: 1,
       adverts: json,
-      componentKey : 0
+      componentKey : 0,
+      orderedItems: [],
+      orderAsc: false
     }),
     methods: {
+      sortItems(orderType) {
+        this.orderAsc = !this.orderAsc;
+        this.orderedItems = _.orderBy(this.adverts.ads, orderType, this.orderAsc ? 'asc' : 'desc')
+      },
       refresh() {
         this.componentKey += 1;
       },
@@ -60,5 +86,9 @@ export default {
   background-color: white;
   padding-left: 10px;
   padding-right: 10px;
+}
+
+.sortContainer {
+  margin-top: 5%;
 }
 </style>
